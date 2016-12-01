c123f70b2f3267e3575a4dcc6d1d72b53331089d
youchan <youchan01@gmail.com>
2016-12-01 14:33:41 +0900

    Handle onKeyDown

--- app/views/todo_view.rb
+++ app/views/todo_view.rb
@@ -3,10 +3,21 @@ class TodoView
 
   state :entries, [ 'Entry 1', 'Entry 2' ]
 
+  def handle_keydown(event)
+    if event.code == 13
+      input = @refs[:entry]
+      unless input.value.empty?
+        @state.entries << input.value
+        input.value = ''
+        force_update
+      end
+    end
+  end
+
   def render
     div({class: 'todo-view'},
       p({class: 'controll'},
-        input({class: 'input', type: 'text', placeholder: 'Input todo'})
+        input({class: 'input', ref: 'entry', type: 'text', placeholder: 'Input todo', onKeyDown: self.method(:handle_keydown)})
       ),
       ul({class: 'todo-list'}, @state.entries.map{|entry| li({}, entry)})
     )
