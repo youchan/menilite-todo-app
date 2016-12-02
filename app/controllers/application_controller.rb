950f522b7412f5aa3afbb27ac3f148c44ddcf8b2
youchan <youchan01@gmail.com>
2016-12-02 11:06:43 +0900

    Authorization for API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < Menilite::Controller
+  before_action(exclude: ['ApplicationController#login', 'User#signup']) do
+    user = User[session[:user_id]]
+    unless user
+      raise Menilite::Unauthorized.new
+    end
+  end
+
   action :login do |username, password|
     user = User.find(name: username)
     if user && user.auth(password)
